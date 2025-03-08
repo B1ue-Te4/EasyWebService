816e632305763f57a1a2f0c22d67cd5ba6c90a88
B1ue-Te4 <103427649+B1ue-Te4@users.noreply.github.com>
2025-03-08 11:23:37 +0000

    cat image generator

---
+++ random-cat/pages/index.tsx
@@ -0,0 +1,36 @@
+import { NextPage } from "next";
+
+const IndexPage: NextPage = () => {
+    return <div>猫画像予定地</div>
+};
+
+export default IndexPage;
+
+type Image = {
+    url: striing;
+};
+
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images: unknown = await res.json();
+    //配列として表現されているか？
+    if (!Array.isArray(images)) {
+        throw new Error("猫の画像が取得できませんでした")
+    }
+    const image: unknown = images[0];
+    // Imageの構造をなしているか？
+    if (!isImage(image)) {
+        throw new Error("猫の画像が取得できませんでした")
+    }
+    return images;
+};
+
+// 型ガード関数
+const isImage = (value: unknown): value is Image => {
+    // 値がオブジェクトなのか？
+    if (!value || typeof value !== "object") {
+        return false;
+    }
+    // urlプロパティが存在し、かつ、それが文字列なのか？
+    return "url" in value && typeof value.url === "string";
+};
